33f5022a9fa2735ec8ed5eb693e84553e6d0019b
Ruben Casado <ruben.casado@altia.es>
2019-05-28 16:30:19 +0200

    working on gallery

--- js/galeria.js
+++ js/galeria.js
@@ -4,6 +4,13 @@
  		console.log("Animal");
  		$(".animal_info").toggle();
  	});
+ 	for(var i=1 ; i<8 ; i++){
+ 		for(var j=1 ; i<5 ; i++){
+ 			var animals_items = '<div class="animal" id="0'+i+'0'+j+'"><img src="img/0'+i+'0'+j+'.jpg"></div>';
+ 			$(".animals_container").append(animals_items);
+ 		}
+ 	}
+
  	$(".close_info").on("click", function(){
  		var animal_id = $(this).attr("id");
  		console.log(animal_id);
