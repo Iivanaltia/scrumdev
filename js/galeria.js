54d76ca060c2b76bdecb9ff1ac3eeddaa6c9d3f5
Ruben Casado <ruben.casado@altia.es>
2019-05-29 14:50:29 +0200

    fixed image of galery

--- js/galeria.js
+++ js/galeria.js
@@ -1,3 +1,7 @@
+var animal = {
+	title: "",
+	class: ""
+}
  $(document).on("ready",function(){
 
  	for(var i=1 ; i<8 ; i++){
@@ -9,16 +13,19 @@
  	$(".animal").on("click", function(){
  		var animal_id = $(this).attr("id");
  		var animalIdx = animal_id.substring(0, 2);
+ 		var idxPhoto = animal_id.substring(2, 4);
  		console.log(animalIdx);
- 		var animal_title = witchAnimal(parseInt(animalIdx));
+ 		var animal = witchAnimal(parseInt(animalIdx), parseInt(idxPhoto));
  		var url = "url('../img/"+animal_id+".jpg')";
  		console.log(url);
- 		console.log(animal_title);
+ 		console.log(animal.title);
+ 		console.log(animal.class);
  		$(".animal_info").toggle();
  		//$(".big_image").css("background-image",url);
- 		$(".big_image").hide();
- 		$("#_"+animalIdx).show();
- 		$("#animal_title").html(animal_title);
+ 		$(".animal_photo_container").children().removeAttr('class');
+ 		$(".animal_photo_container").children().addClass("big_image "+ animal.class);
+
+ 		$("#animal_title").html(animal.title);
  	});
 
  	$(".close_info").on("click", function(){
@@ -28,29 +35,51 @@
  	})
  });
 
- function witchAnimal(animalIdx){
+ function witchAnimal(animalIdx,idxPhoto){
+ 	console.log(idxPhoto);
  	switch(animalIdx){
  		case 1:
- 			animal_title = "León";
+ 			animal = {
+ 				title : "León",
+ 				class : "Leon"+idxPhoto
+ 			}
  			break;
  		case 2:
- 			animal_title = "Canguro";
+ 			animal = {
+ 				title : "Canguro",
+ 				class : "Canguro"+idxPhoto
+ 			}
  			break;
  		case 3:
- 			animal_title = "Koala";
+ 			animal = {
+ 				title : "Koala",
+ 				class : "Koala"+idxPhoto
+ 			}
  			break;
  		case 4:
- 			animal_title = "Cocodrilo";
+ 			animal = {
+ 				title : "Cocodrilo",
+ 				class : "Cocodrilo"+idxPhoto
+ 			}
  			break;
  		case 5:
- 			animal_title = "Tiburón";
+ 			animal = {
+ 				title : "Tiburón",
+ 				class : "Tiburón"+idxPhoto
+ 			}
  			break;
  		case 6:
- 			animal_title = "Pingüino";
+ 			animal = {
+ 				title : "Pingüino",
+ 				class : "Pingüino"+idxPhoto
+ 			}
  			break;
  		case 7:
- 			animal_title = "Loro";
+ 			animal = {
+ 				title : "Loro",
+ 				class : "Loro"+idxPhoto
+ 			}
  			break;
  	}
- 	return animal_title;
+ 	return animal;
  }
